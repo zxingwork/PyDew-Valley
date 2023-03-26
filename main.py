66957318469632c0c055cc2a747abfc80eabc04a
xingz <zxingwork@163.com>
2023-03-26 12:49:34 +0800

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+import sys
+from setting import *
+from level import Level
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Sprout land")
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick(60) / 1000
+            self.level.run(dt)
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ player.py
@@ -0,0 +1,57 @@
+import pygame
+from setting import *
+from typing import *
+from math import sqrt
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
+
+        # general setup
+        self.image = pygame.Surface((64, 32))
+        self.image.fill('green')
+        self.rect = self.image.get_rect(center=pos)
+
+        # movement attribute
+        self.direction = pygame.math.Vector2(0, 0)
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def move(self, dt):
+        if self.direction.x or self.direction.y:
+            self.direction = self.direction.normalize()
+
+        # horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        # vertical movement
+        self.pos.y = self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
+
+        print(self.direction)
+
+        # exist if self.direction is None
+
+
+    def update(self, dt) -> None:
+        self.input()
+        self.move(dt)
