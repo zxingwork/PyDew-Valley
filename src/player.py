48f8c8dc36ef4a0480276cba1f367e23b4e1eb49
zhangxing <zxingwork@163.com>
2023-03-26 21:25:50 +0800

    add

--- src/player.py
+++ src/player.py
@@ -3,24 +3,47 @@ from setting import *
 from typing import *
 from math import sqrt
 from support import *
+from timer import Timer
+
+
+class PlayerStatus:
+    up = 'up'
+    down = 'down'
+    right = 'right'
+    left = 'left'
+    up_idle = 'up_idle'
+    down_idle = 'down_idle'
+    left_idle = 'left_idle'
+    right_idle = 'right_idle'
+    up_axe = 'up_axe'
+    down_axe = 'down_axe'
+    left_axe = 'left_axe'
+    right_axe = 'right_axe'
+    up_water = 'up_water'
+    down_water = 'down_water'
+    right_water = 'right_water'
+    left_water = 'left_water'
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
-
+        self.animations = {}
         self.import_assets()
+        self.status = PlayerStatus.up
+        self.frame_index = 0
 
         # general setup
-        self.image = pygame.Surface((100, 100))
-        self.image.fill('green')
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
 
         # movement attribute
         self.direction = pygame.math.Vector2(0, 0)
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
-        self.animations = {}
+
+        # tools
+        self.selected_tool = 'axe'
 
     def import_assets(self):
         self.animations = {'up': [], 'down': [], 'right': [], 'left': [],
@@ -31,24 +54,48 @@ class Player(pygame.sprite.Sprite):
         for animation in self.animations.keys():
             full_path = '../graphics/character/' + animation
             self.animations[animation] = import_folder(full_path)
-            pass
+
+        print(self.animations)
+
+    def animate(self, dt):
+        self.frame_index += len(self.animations[self.status]) * dt
+        if self.frame_index > len(self.animations[self.status]):
+            self.frame_index = 0
+        self.image = self.animations[self.status][int(self.frame_index)]
 
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = PlayerStatus.up
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = PlayerStatus.down
         else:
             self.direction.y = 0
 
         if keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = PlayerStatus.left
         elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = PlayerStatus.right
         else:
             self.direction.x = 0
+
+        # tool use
+        if keys[pygame.K_SPACE]:
+            # timer for the tool use
+
+            pass
+
+    def get_status(self):
+        # if the player is not moving
+        # add _idle to the status
+        if self.direction.magnitude() == 0:
+            if self.status + "_idle" in self.animations:
+                self.status += '_idle'
 
     def move(self, dt):
         if self.direction.x or self.direction.y:
@@ -64,4 +111,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt) -> None:
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
--- src/support.py
+++ src/support.py
@@ -1,10 +1,15 @@
 from os import walk
+import os
+import pygame
 
 
 def import_folder(path):
     surface_list = []
-
-    for folder in walk(path):
-        print(folder)
+    for _, _, img_files in walk(path):
+        for img in img_files:
+            full_path = os.path.join(path, img)
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+            pass
 
     return surface_list
